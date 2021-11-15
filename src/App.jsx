d8d297b9a0d0444f87523cb0b2edcbe0c81c799c
james-totty <james.totty@speechlink.co.uk>
2021-11-15 19:40:55 +0000

    :tada: It's alive!

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Another from './Another';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const decrement = () => {
+    setCount(count - 1);
+  }
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+  const someStyle = {
+    background: 'blue',
+    color: 'white',
+    fontSize: '28px',
+    fontWeight: 'bold',
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <Another name="James" />
+        <div>
+          <span>{count}</span>
+          <button onClick={decrement}>-</button>
+          <button onClick={increment}>+</button>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p style={someStyle}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
